0a1add12d2c3c97153c0dc74ed6f519766504957
MrBaykal <ebaykal2266@gmail.com>
2024-07-27 09:48:18 +0200

    app.py updated

--- src/app.py
+++ src/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_docker():
-    return 'Merhaba, Dünya! Hello World, This is from MrBaykal '
+    return 'Merhaba, Dünya! Hello World, This is from MrBaykal. It is a lovely DevOps Journey '
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
